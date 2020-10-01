fe7484d47b0a16f8e026b6a461db2188decc36d2
Kelvin <duchung97dl@gmail.com>
2020-10-01 23:31:24 +1000

    Implement CRUd operation for post.

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -13,10 +13,44 @@ export class PostResolver {
 
   // Query a single post
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id", () => Int) id: number,
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  // Mutation to create a single post
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = await em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    return em.findOne(Post, { id });
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
   }
 }
